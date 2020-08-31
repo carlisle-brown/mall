c64ccc9c22cd6fa58869bd840ad436a6144e9a90
Carlisle <carlisle9797@outlook.com>
2020-08-31 11:39:09 +0800

    收货地址模块

---
+++ src/main/java/com/carlisle/brown/mall/service/impl/ShippingServiceImpl.java
@@ -0,0 +1,71 @@
+package com.carlisle.brown.mall.service.impl;
+
+import com.carlisle.brown.mall.common.ServerResponse;
+import com.carlisle.brown.mall.dao.ShippingMapper;
+import com.carlisle.brown.mall.pojo.Shipping;
+import com.carlisle.brown.mall.service.IShippingService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.google.common.collect.Maps;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service("iShippingService")
+public class ShippingServiceImpl implements IShippingService {
+
+    private ShippingMapper shippingMapper;
+
+    public ShippingServiceImpl(ShippingMapper shippingMapper) {
+        this.shippingMapper = shippingMapper;
+    }
+
+    @Override
+    public ServerResponse add(Integer userId, Shipping shipping) {
+        shipping.setUserId(userId);
+        int rowCount = shippingMapper.insert(shipping);
+        if(rowCount > 0){
+            Map result = Maps.newHashMap();
+            result.put("shippingId",shipping.getId());
+            return ServerResponse.createBySuccess("新建地址成功",result);
+        }
+        return ServerResponse.createByErrorMessage("新建地址失败");
+    }
+
+    @Override
+    public ServerResponse<String> del(Integer userId, Integer shippingId) {
+        int resultCount = shippingMapper.deleteByShippingIdUserId(userId,shippingId);
+        if(resultCount > 0){
+            return ServerResponse.createBySuccess("删除地址成功");
+        }
+        return ServerResponse.createByErrorMessage("删除地址失败");
+    }
+
+    @Override
+    public ServerResponse update(Integer userId, Shipping shipping) {
+        shipping.setUserId(userId);
+        int rowCount = shippingMapper.updateByShipping(shipping);
+        if(rowCount > 0){
+            return ServerResponse.createBySuccess("更新地址成功");
+        }
+        return ServerResponse.createByErrorMessage("更新地址失败");
+    }
+
+    @Override
+    public ServerResponse<Shipping> select(Integer userId, Integer shippingId) {
+        Shipping shipping = shippingMapper.selectByShippingIdUserId(userId,shippingId);
+        if(shipping == null){
+            return ServerResponse.createByErrorMessage("无法查询到该地址");
+        }
+        return ServerResponse.createBySuccess("更新地址成功",shipping);
+    }
+
+    @Override
+    public ServerResponse<PageInfo> list(Integer userId, int pageNum, int pageSize) {
+        PageHelper.startPage(pageNum,pageSize);
+        List<Shipping> shippingList = shippingMapper.selectByUserId(userId);
+        PageInfo pageInfo = new PageInfo(shippingList);
+        return ServerResponse.createBySuccess(pageInfo);
+    }
+}
